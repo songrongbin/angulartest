34a50181b101ec82bc98cd6f1d10793508eb378b
songrongbin(宋荣彬.技术中心.服务研发部) <songrongbin@ds.gome.com.cn>
2017-02-28 16:58:10 +0800

    init project

---
+++ js/controllers.js
@@ -0,0 +1,14 @@
+var phoneCatApp = angular.module('phoneCatApp', []);
+
+phoneCatApp.controller("PhoneListCtrl", function($scope) {
+    $scope.phones = [
+        {"name": "Nexus S",
+            "snippet": "Fast just got faster with Nexus S."},
+        {"name": "Motorola XOOM? with Wi-Fi",
+            "snippet": "The Next, Next Generation tablet."},
+        {"name": "MOTOROLA XOOM?",
+            "snippet": "The Next, Next Generation tablet."}
+    ];
+
+    $scope.hello = "Hello, World!";
+});
